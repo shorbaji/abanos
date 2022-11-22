0666088f48e18dae309313846343d26e9deadd29
Omar Shorbaji <omar.shorbaji@gmail.com>
2022-11-21 19:42:02 -0500

    initial expr enum type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+enum expr {
+    NIL,
+    Number(i64),
+    Symbol(String)
+}
+
 fn main() {
+    let e1: expr = expr::NIL;
+    let e2: expr = expr::Number(7);
+    let e3: expr = expr::Number(6);
+    let e4: expr = expr::Symbol(String::from("+"));
+
+    
     println!("Hello, world!");
 }
