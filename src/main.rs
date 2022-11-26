096297a3468ca1f5f88d8a99a1b1e2a7ca66a7ad
Omar Shorbaji <omar.shorbaji@gmail.com>
2022-11-25 20:16:13 -0500

    r7rs section 4.1 - primitive expression types

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,159 @@
 use core::fmt;
+use std::collections::HashMap;
 
 enum Proc {
     Builtin,
-    Defined,
+    Defined(Box<Expr>, Vec<Box<Expr>>),
 }
 
+enum Boolean {
+    True,
+    False,
+}
 enum Value {
-    NIL,
-    Proc(Proc),
-    Number(i64),
-    String(String),
+    Boolean(Boolean),
+    Bytevector,
     Char(char),
+    Eof,
+    NIL,
+    Number(Number),
+    Pair(Box<Value>, Box<Value>),
+    Port,
+    Proc(Proc),
+    String(String),
+    Symbol(String),
+    Unspecified,
+    Vector,
 }
 enum Expr {
-    NIL,
-    Number(i64),
-    Symbol(String),
-    Char(char),
-    Pair(Box<Expr>, Box<Expr>),
+    VariableReferenceExpr(String),
+    LiteralExpr(Literal),
+    ProcedureCallExpr(Box<Expr>, Vec<Box<Expr>>),
+    LambdaExpr(Box<Expr>, Vec<Box<Expr>>),
+    ConditionalExpr(Box<Expr>, Box<Expr>, Box<Expr>),
+    AssignmentExpr(Box<Expr>, Box<Expr>),
+    IncludeExpr, // To be implemented
 }
+
+enum Literal {
+    Boolean(Boolean),
+    Bytevector,
+    Char(char),
+    Number(Number),
+    String(String),
+    Vector,
+}
+
+struct Env {
+    hash: HashMap<String, Value>,
+    parent: Option<Box<Env>>,
+}
+
+type Location = i64;
+type Number = i64;
 
 impl fmt::Display for Value {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let str = match self {
+            Value::Boolean(b) => format!("{}", match b {
+                Boolean::True => true,
+                Boolean::False => false,
+            }),
+            Value::Bytevector => String::from("bytevector"),
+            Value::Char(c) => format!("{}", c),
+            Value::Eof => String::from("eof"),            
             Value::NIL => String::from("nil"),
             Value::Number(n) => n.to_string(),
-            Value::String(s) => s.clone(),
-            Value::Char(c) => format!("{}", c),
+            Value::Pair(a, b) => format!("({} . {})", *a, *b),
+            Value::Port => String::from("port"),
             Value::Proc(_) => String::from("proc"),
+            Value::String(s) => format!("\"{}\"", s),
+            Value::Symbol(s) => s.clone(),
+            Value::Unspecified => String::from("unspecified"),
+            Value::Vector => String::from("vector"),        
         };
 
         write!(f, "{}", str)
     }
 }
 
-fn cons(a: Expr, b: Expr) -> Expr {
-    Expr::Pair(Box::new(a), Box::new(b))
-}
+// fn cons(a: Expr, b: Expr) -> Expr {
+//     Expr::Pair(Box::new(a), Box::new(b))
+// }
 
-fn car(e: Expr) -> Result<Expr, &'static str> {
-    match e {
-        Expr::Pair(a, _) => Ok(*a),
-        _ => Err("not a pair"),
+// fn car(e: Expr) -> Result<Expr, &'static str> {
+//     match e {
+//         Expr::Pair(a, _) => Ok(*a),
+//         _ => Err("not a pair"),
+//     }
+// }
+
+// fn cdr(e: Expr) -> Result<Expr, &'static str> {
+//     match e {
+//         Expr::Pair(_, b) => Ok(*b),
+//         _ => Err("not a pair"),
+//     }
+// }
+
+fn self_eval(l: Literal) -> Value {
+    match l {
+        Literal::Boolean(b) => Value::Boolean(b),
+        Literal::Bytevector => Value::Bytevector,
+        Literal::Char(c) => Value::Char(c),
+        Literal::Number(n) => Value::Number(n),
+        Literal::String(s) => Value::String(s.clone()),
+        Literal::Vector => Value::Vector,
     }
 }
 
-fn cdr(e: Expr) -> Result<Expr, &'static str> {
-    match e {
-        Expr::Pair(_, b) => Ok(*b),
-        _ => Err("not a pair"),
-    }
+fn lookup(v: String, env: &Env) -> Result<Value, &'static str>{
+    Err("variable could not be found")
 }
 
-fn eval(e: Expr) -> Result<Value, &'static str> {
-    match e {
-        Expr::NIL => Ok(Value::NIL),
-        Expr::Number(n) => Ok(Value::Number(n)),
-        Expr::Char(c) => Ok(Value::Char(c)),
-        Expr::Symbol(s) => Err("symbol not found"),
-        Expr::Pair(_, _) => Err("Don't know how to deal with that"),
+fn apply(proc: Value, operands: Vec<Box<Value>>) -> Result<Value, &'static str> {
+    Ok(Value::NIL)
+}
 
+fn evlis(operands: Vec<Box<Expr>>, env: &Env) -> Vec<Box<Value>> {
+    vec![]
+}
+
+fn is_true(v: Value) -> bool {
+    match v {
+        Value::Boolean(b) => match b {
+            Boolean::True => true,
+            Boolean::False => false,
+        },
+        Value::NIL => false,
+        _ => true,
+    }
+}
+fn eval(e: Expr, env: &Env) -> Result<Value, &'static str> {
+    match e {
+        Expr::LiteralExpr(l) => Ok(self_eval(l)),
+        Expr::VariableReferenceExpr(v) => lookup(v, env),
+        Expr::ProcedureCallExpr(operator, operands) => {
+            match eval(* operator, env) {
+                Ok(v) => apply(v, evlis(operands, env)),
+                Err(s) => Err(s)
+            }
+        },
+        Expr::LambdaExpr(formals, body)
+            =>  Ok(Value::Proc(Proc::Defined(formals, body))),
+        Expr::ConditionalExpr(test, consequent, alternate) => {
+            match eval(*test, env) {
+                Ok(v) => match is_true(v) {
+                    true => eval(*consequent, env),
+                    false => eval(*alternate, env),
+                }
+                Err(e) => Err(e)
+            }
+            
+        },
+        Expr::AssignmentExpr(var, target) =>
+            // need to implement assigment here
+            Ok(Value::Unspecified),
+        Expr::IncludeExpr => Ok(Value::Unspecified),
     }
 }
 
@@ -70,13 +164,11 @@ fn print(r: Result<Value, &'static str>) {
     }
 }
 fn main() {
-    let e1: Expr = Expr::NIL;
-    let e2: Expr = Expr::Number(7);
-    let e3: Expr = Expr::Number(6);
-    let e4: Expr = Expr::Symbol(String::from("+"));
-    let e5: Expr = cons(e2,
-                        cons(e3, e1));
+    let global_env = Env {
+        hash: HashMap::new(),
+        parent: Option::None,
+    };
 
-    print(eval(e4));
-    print(eval(e5));
+    let e = Expr::LiteralExpr(Literal::Number(7));
+    print(eval(e, &global_env));
 }
