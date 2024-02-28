23f7d3b381d64419beaac8791eb3a5c43dc58bc6
Omar Shorbaji <github@shorbaji.com>
2024-02-27 22:57:52 -0500

    code cleanup

--- src/token.rs
+++ src/token.rs
@@ -1,5 +1,5 @@
 /// This module gets an ID token for the CLI from the server.
-/// usage: 
+/// usage:
 ///
 /// get_token("<host>".to_string());
 ///
@@ -7,8 +7,8 @@ use rouille::{Response, Server}; // needed to set up a local server to serve a r
 use std::path::PathBuf; // needed to save the token to a file
 use std::sync::mpsc; // needed for the local server to communicate with the main thread
 
-
-const GOOGLE_CLIENT_ID: &str = "169688466353-b2g33gq8h5k0b73b5h155lggoarcaphs.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID: &str =
+    "169688466353-b2g33gq8h5k0b73b5h155lggoarcaphs.apps.googleusercontent.com";
 
 pub fn get_token() -> Result<String, String> {
     // get or create the path to ~/.abanos
@@ -52,21 +52,18 @@ fn login() -> Result<String, String> {
     // we either open a browser with the auth login url OR
     // we ask the user to open a browser with a url to login and enter the resulting code
     // either way we need a base url to start with
-    login_with_browser()
-        .or_else(|_| login_without_browser())
+    login_with_browser().or_else(|_| login_without_browser())
 }
 
 fn login_with_browser() -> Result<String, String> {
     let (tx, rx) = mpsc::channel();
 
     // we create the server without running it first so we can get the port
-    let server = Server::new(
-                    "localhost:6871",
-                    move |request| handler(request, tx.clone()))
-                    .map_err(|e| format!("rouille error: {:?}", e))?;
-    let addr = server
-                .server_addr()
-                .port();
+    let server = Server::new("localhost:6871", move |request| {
+        handler(request, tx.clone())
+    })
+    .map_err(|e| format!("rouille error: {:?}", e))?;
+    let addr = server.server_addr().port();
 
     // we use the port as part of the redirect url
     // let url = format!("{url_base}?signInSuccessUrl=http://localhost:{addr}");
@@ -106,12 +103,14 @@ fn handler(request: &rouille::Request, tx: mpsc::Sender<String>) -> rouille::Res
         }
     } else {
         // extract the jwt from the hash/fragement and redirect to a url with the jwt as a query param
-        Response::html("<script>
+        Response::html(
+            "<script>
             let loc = window.location;
             let hash = loc.hash;
             let jwt = hash.split('&').find((s) => s.startsWith('id_token=')).split('=')[1];
             window.location.replace(`http://localhost:6871/?jwt=${jwt}`);
-        </script>")
+        </script>",
+        )
     }
 }
 
@@ -133,12 +132,13 @@ fn login_without_browser() -> Result<String, String> {
     }
 }
 
-
 use std::process::exit;
 
 use openidconnect::core::{CoreClient, CoreProviderMetadata, CoreResponseType};
 use openidconnect::reqwest::http_client;
-use openidconnect::{AuthenticationFlow, ClientId, CsrfToken, IssuerUrl, Nonce, RedirectUrl, Scope};
+use openidconnect::{
+    AuthenticationFlow, ClientId, CsrfToken, IssuerUrl, Nonce, RedirectUrl, Scope,
+};
 
 fn handle_error<T: std::error::Error>(fail: &T, msg: &'static str) {
     let mut err_msg = format!("ERROR: {}", msg);
@@ -152,7 +152,6 @@ fn handle_error<T: std::error::Error>(fail: &T, msg: &'static str) {
 }
 
 fn get_authorization_url(redirect_url: Option<String>) -> String {
-
     let google_client_id = ClientId::new(GOOGLE_CLIENT_ID.to_string());
     let issuer_url =
         IssuerUrl::new("https://accounts.google.com".to_string()).expect("Invalid issuer URL");
@@ -165,20 +164,10 @@ fn get_authorization_url(redirect_url: Option<String>) -> String {
 
     // Set up the config for the Google OAuth2 process.
     let client = if let Some(redirect_url) = redirect_url {
-        CoreClient::from_provider_metadata(
-            provider_metadata,
-            google_client_id,
-            None,
-        )
-        .set_redirect_uri(
-            RedirectUrl::new(redirect_url).expect("Invalid redirect URL"),
-        )
+        CoreClient::from_provider_metadata(provider_metadata, google_client_id, None)
+            .set_redirect_uri(RedirectUrl::new(redirect_url).expect("Invalid redirect URL"))
     } else {
-        CoreClient::from_provider_metadata(
-            provider_metadata,
-            google_client_id,
-            None,
-        )
+        CoreClient::from_provider_metadata(provider_metadata, google_client_id, None)
     };
 
     // Generate the authorization URL to which we'll redirect the user.
@@ -193,5 +182,4 @@ fn get_authorization_url(redirect_url: Option<String>) -> String {
         .url();
 
     authorize_url.to_string()
-
 }
