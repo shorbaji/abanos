56a22d42b63945b7ac037498f5e0091f1e15a939
Omar Shorbaji <github@shorbaji.com>
2024-02-27 22:42:07 -0500

    switch to Google authentication & version bump

--- src/token.rs
+++ src/token.rs
@@ -60,7 +60,7 @@ fn login_with_browser(url_base: &String) -> Result<String, String> {
 
     // we create the server without running it first so we can get the port
     let server = Server::new(
-                    "localhost:0",
+                    "localhost:6871",
                     move |request| handler(request, tx.clone()))
                     .map_err(|e| format!("rouille error: {:?}", e))?;
     let addr = server
@@ -68,7 +68,8 @@ fn login_with_browser(url_base: &String) -> Result<String, String> {
                 .port();
 
     // we use the port as part of the redirect url
-    let url = format!("{url_base}?signInSuccessUrl=http://localhost:{addr}");
+    // let url = format!("{url_base}?signInSuccessUrl=http://localhost:{addr}");
+    let url = get_authorization_url(addr);
 
     // try to open the browser with the url
     match open::that(url.as_str()) {
@@ -98,14 +99,19 @@ fn handler(request: &rouille::Request, tx: mpsc::Sender<String>) -> rouille::Res
     // once a request comes in - look for the jwt param and send it to the main thread
     if let Some(jwt) = request.get_param("jwt") {
         match tx.send(jwt) {
-            Ok(_) => Response::text("Login successful. You can close this tab now."),
+            Ok(_) => Response::text("Login successful. You can close this tab."),
             Err(e) => Response::text(format!("mpsc channel error: {:?}", e)).with_status_code(500),
         }
     } else {
-        Response::text("No jwt found").with_status_code(400)
+        // extract the jwt from the hash/fragement and redirect to a url with the jwt as a query param
+        Response::html("<script>
+            let loc = window.location;
+            let hash = loc.hash;
+            let jwt = hash.split('&').find((s) => s.startsWith('id_token=')).split('=')[1];
+            window.location.replace(`http://localhost:6871/?jwt=${jwt}`);
+        </script>")
     }
 }
-
 
 fn login_without_browser(url_base: &String) -> Result<String, String> {
     // if no browser, provide the user with a URL to open in their browser
@@ -123,4 +129,129 @@ fn login_without_browser(url_base: &String) -> Result<String, String> {
         Ok(_) => Ok(jwt),
         Err(e) => Err(format!("stdin error: {:?}", e)),
     }
+}
+
+
+use std::env;
+use std::io::{BufRead, BufReader, Write};
+use std::net::TcpListener;
+use std::process::exit;
+
+use serde::{Deserialize, Serialize};
+
+use openidconnect::core::{
+    CoreAuthDisplay, CoreClaimName, CoreClaimType, CoreClient, CoreClientAuthMethod, CoreGrantType,
+    CoreIdTokenClaims, CoreIdTokenVerifier, CoreJsonWebKey, CoreJsonWebKeyType, CoreJsonWebKeyUse,
+    CoreJweContentEncryptionAlgorithm, CoreJweKeyManagementAlgorithm, CoreJwsSigningAlgorithm,
+    CoreResponseMode, CoreResponseType, CoreRevocableToken, CoreSubjectIdentifierType,
+};
+use openidconnect::reqwest::http_client;
+use openidconnect::{
+    AdditionalProviderMetadata, AuthenticationFlow, AuthorizationCode, ClientId, ClientSecret,
+    CsrfToken, IssuerUrl, Nonce, OAuth2TokenResponse, ProviderMetadata, RedirectUrl, RevocationUrl,
+    Scope,
+};
+
+fn handle_error<T: std::error::Error>(fail: &T, msg: &'static str) {
+    let mut err_msg = format!("ERROR: {}", msg);
+    let mut cur_fail: Option<&dyn std::error::Error> = Some(fail);
+    while let Some(cause) = cur_fail {
+        err_msg += &format!("\n    caused by: {}", cause);
+        cur_fail = cause.source();
+    }
+    println!("{}", err_msg);
+    exit(1);
+}
+
+// Teach openidconnect-rs about a Google custom extension to the OpenID Discovery response that we can use as the RFC
+// 7009 OAuth 2.0 Token Revocation endpoint. For more information about the Google specific Discovery response see the
+// Google OpenID Connect service documentation at: https://developers.google.com/identity/protocols/oauth2/openid-connect#discovery
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct RevocationEndpointProviderMetadata {
+    revocation_endpoint: String,
+}
+impl AdditionalProviderMetadata for RevocationEndpointProviderMetadata {}
+type GoogleProviderMetadata = ProviderMetadata<
+    RevocationEndpointProviderMetadata,
+    CoreAuthDisplay,
+    CoreClientAuthMethod,
+    CoreClaimName,
+    CoreClaimType,
+    CoreGrantType,
+    CoreJweContentEncryptionAlgorithm,
+    CoreJweKeyManagementAlgorithm,
+    CoreJwsSigningAlgorithm,
+    CoreJsonWebKeyType,
+    CoreJsonWebKeyUse,
+    CoreJsonWebKey,
+    CoreResponseMode,
+    CoreResponseType,
+    CoreSubjectIdentifierType,
+>;
+
+fn get_authorization_url(port: u16) -> String {
+
+    let google_client_id = ClientId::new(
+        env::var("GOOGLE_CLIENT_ID").expect("Missing the GOOGLE_CLIENT_ID environment variable."),
+    );
+    let issuer_url =
+        IssuerUrl::new("https://accounts.google.com".to_string()).expect("Invalid issuer URL");
+
+    // Fetch Google's OpenID Connect discovery document.
+    //
+    // Note: If we don't care about token revocation we can simply use CoreProviderMetadata here
+    // instead of GoogleProviderMetadata. If instead we wanted to optionally use the token
+    // revocation endpoint if it seems to be supported we could do something like this:
+    //   #[derive(Clone, Debug, Deserialize, Serialize)]
+    //   struct AllOtherProviderMetadata(HashMap<String, serde_json::Value>);
+    //   impl AdditionalClaims for AllOtherProviderMetadata {}
+    // And then test for the presence of "revocation_endpoint" in the map returned by a call to
+    // .additional_metadata().
+
+    let provider_metadata = GoogleProviderMetadata::discover(&issuer_url, http_client)
+        .unwrap_or_else(|err| {
+            handle_error(&err, "Failed to discover OpenID Provider");
+            unreachable!();
+        });
+
+    let revocation_endpoint = provider_metadata
+        .additional_metadata()
+        .revocation_endpoint
+        .clone();
+    println!(
+        "Discovered Google revocation endpoint: {}",
+        revocation_endpoint
+    );
+
+    // Set up the config for the Google OAuth2 process.
+    let client = CoreClient::from_provider_metadata(
+        provider_metadata,
+        google_client_id,
+        None,
+    )
+    // This example will be running its own server at localhost:8080.
+    // See below for the server implementation.
+    .set_redirect_uri(
+        RedirectUrl::new(format!("http://localhost:{port}")).expect("Invalid redirect URL"),
+    )
+    // Google supports OAuth 2.0 Token Revocation (RFC-7009)
+    .set_revocation_uri(
+        RevocationUrl::new(revocation_endpoint).expect("Invalid revocation endpoint URL"),
+    );
+
+
+    // Generate the authorization URL to which we'll redirect the user.
+    let (authorize_url, csrf_state, nonce) = client
+        .authorize_url(
+            AuthenticationFlow::<CoreResponseType>::Implicit(true),
+            CsrfToken::new_random,
+            Nonce::new_random,
+        )
+        // This example is requesting access to the "calendar" features and the user's profile.
+        .add_scope(Scope::new("email".to_string()))
+        .add_scope(Scope::new("profile".to_string()))
+        .url();
+
+    authorize_url.to_string()
+
 }
